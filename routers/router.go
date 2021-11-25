46219511e395de9754488427df201f06e44731a7
Azerrroth <732814312@qq.com>
2021-11-25 22:26:39 +0800

    Update: router.

--- routers/router.go
+++ routers/router.go
@@ -34,21 +34,42 @@ func InitRouter() *gin.Engine {
 	// apiv1.Use(session.Session())
 	apiv1.Use(jwt.JWT())
 	{
-		apiv1.POST("/dorm", v1.GetDormList)
-		apiv1.GET("/buildings", v1.GetBuildingList)
-		apiv1.GET("/buildingStatus", v1.GetBuildingStatus)
-		apiv1.GET("/buildingsStatus", v1.GetBuildingsStatus)
-		apiv1.GET("/user2RoomInfo", v1.GetUser2RoomInfo)
-
 		apiv1.GET("/token", func(c *gin.Context) {
 			c.JSON(200, gin.H{
 				"message": "token access",
 			})
 		})
 
+		dorm := apiv1.Group("/dorm")
+
+		// Dorm route function
+		dorm.GET("/user2RoomInfo", v1.GetUser2RoomInfo)
+		dorm.GET("/buildings", v1.GetBuildingList)
+		dorm.GET("/buildingStatus", v1.GetBuildingStatus)
+		dorm.GET("/buildingsStatus", v1.GetBuildingsStatus)
+		dorm.POST("/checkOutRoom", v1.CheckOutRoom)
+
+		// apiv1.POST("/dorm", v1.GetDormList)
+		apiv1.GET("/buildings", v1.GetBuildingList)
+		apiv1.GET("/buildingStatus", v1.GetBuildingStatus)
+		apiv1.GET("/buildingsStatus", v1.GetBuildingsStatus)
+		apiv1.GET("/user2RoomInfo", v1.GetUser2RoomInfo)
+		apiv1.POST("/checkOutRoom", v1.CheckOutRoom)
+
+		user := apiv1.Group("/user")
+
+		// user route function
+		user.POST("/updateUserProfile", v1.UpdateUserProfile)
+		user.GET("/updateCertifyCode", v1.UpdateCertifyCode)
+
 		apiv1.POST("/updateUserProfile", v1.UpdateUserProfile)
 		apiv1.GET("/updateCertifyCode", v1.UpdateCertifyCode)
-		apiv1.POST("/checkOutRoom", v1.CheckOutRoom)
+
+		order := apiv1.Group("/order")
+
+		// order route function
+		order.POST("/bookOrder", v1.BookOrder)
+
 		apiv1.POST("/bookOrder", v1.BookOrder)
 	}
 
